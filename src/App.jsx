d4be70438d1a247f202d12bda3e534f535609a3b
teriTheDesigner <tere1109@stud.kea.dk>
2024-03-29 13:25:28 +0100

    name changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import Spline from "@splinetool/react-spline";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <div className="">
       <Spline
         className="splineMobile"
         scene="https://prod.spline.design/VX0aVbxGbjo6hycq/scene.splinecode"
@@ -16,7 +14,7 @@ function App() {
         className="spline"
         scene="https://prod.spline.design/QG1tJLenXJNR1K4p/scene.splinecode"
       />
-    </>
+    </div>
   );
 }
 
